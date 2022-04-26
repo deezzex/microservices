ba66687f2aacec1a17694a3c8cd048f0e742af36
Sviatoslav Pshtir <sviatoslav.pshtir@nerdysoft.com>
2022-04-26 15:21:04 +0300

    Added Javadoc for registry service

--- service-registry/src/main/java/com/deezzex/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/deezzex/ServiceRegistryApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Major class in spring boot application which run this service
+ * @version 1
+ * @author Sviatoslav Pshtir
+ **/
+
 @EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegistryApplication {
